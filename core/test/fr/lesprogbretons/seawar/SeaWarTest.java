62900c63c4c78a835547e6f6146b37fb52b07230
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2017-12-05 19:39:15 +0100

    Ajout tests à la CI

---
+++ core/test/fr/lesprogbretons/seawar/SeaWarTest.java
@@ -0,0 +1,11 @@
+package fr.lesprogbretons.seawar;
+
+import org.junit.Test;
+
+public class SeaWarTest {
+
+    @Test
+    public void passingTest() throws Exception {
+        assert true;
+    }
+}
