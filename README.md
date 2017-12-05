bf75e7c7eb37b94245daf49a6623b994d8a242ba
Bastien Hoffstetter <bastien.hoffstetter@free.fr>
2017-12-05 18:04:36 +0100

    Mise en place du stack technique

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SeaWar
 Le code source du jeu SeaWar est disponible dans ce repository
+
+# Les Programmeurs Bretons
+Équipe composée de : 
+- Deroo Antoine
+- Della Libera Paul
+- Jean Raphaël
+- Lassalle Thomas
+- Baron Ewen
+- Hoffstetter Bastien
---
+++ desktop/src/fr/lesprogbretons/seawar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.lesprogbretons.seawar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.lesprogbretons.seawar.SeaWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SeaWar(), config);
+	}
+}
